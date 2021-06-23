61c028caefdc4af8c10ecdc66b0f09f7c13a44c5
Stephanie Smith <stephanie.c.smith@jpl.nasa.gov>
2021-06-23 15:00:01 -0700

    uses branch from www that removes component scoping from BlockImageCarouselItem

--- www-sync.config.js
+++ www-sync.config.js
@@ -23,7 +23,7 @@ module.exports = {
     'ArticleCarousel',
     'ArticleCarouselItem',
     'BaseButton',
-    'BaseCarouselCards', // imports swiper
+    'BaseCarouselCards',
     'BaseHeading',
     'BaseImage',
     'BaseImageCaption',
@@ -33,8 +33,8 @@ module.exports = {
     'BlockIframeEmbed',
     'BlockInlineImage',
     'BlockImage',
-    'BlockImageCarousel', // imports swiper
-    'BlockImageCarouselItem', // scoped
+    'BlockImageCarousel',
+    'BlockImageCarouselItem',
     'BlockImageGallery',
     'BlockKeyPoints',
     'BlockQuote',
