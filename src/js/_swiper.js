f594530c7b37bdc1d57e886aeec25c6190e9d099
Stephanie Smith <stephanie.c.smith@jpl.nasa.gov>
2021-09-01 15:39:04 -0700

    updating caniuse-lite, updating _swiper.js to provide exportable options

--- src/js/_swiper.js
+++ src/js/_swiper.js
@@ -7,73 +7,77 @@ import SwiperCore, { Navigation, Pagination, Lazy, A11y } from 'swiper/core'
 SwiperCore.use([Navigation, Lazy, A11y, Pagination])
 
 // BaseCarouselCards
+export const BaseCarouselCardsOptions = {
+  slidesPerView: 1.25,
+  speed: 500,
+  threshold: 20, // swipe threshold in px
+  spaceBetween: 28,
+  watchOverflow: true,
+  watchSlidesProgress: true,
+  watchSlidesVisibility: true,
+  loop: false,
+  preloadImages: false,
+  lazy: {
+    loadPrevNext: true,
+    loadOnTransitionStart: true,
+    loadPrevNextAmount: 2,
+  },
+  navigation: {
+    nextEl: '.swiper-next',
+    prevEl: '.swiper-prev',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 2,
+      spaceBetween: 56,
+    },
+  },
+  on: {
+    init(swiper) {
+      swiper.$el.removeClass('opacity-0')
+      swiper.$el.addClass('opacity-100')
+    },
+  },
+}
+
 const BaseCarouselCards = new SwiperCore(
   '.BaseCarouselCards .swiper-container',
-  {
-    slidesPerView: 1.25,
-    speed: 500,
-    threshold: 20, // swipe threshold in px
-    spaceBetween: 28,
-    watchOverflow: true,
-    watchSlidesProgress: true,
-    watchSlidesVisibility: true,
-    loop: false,
-    preloadImages: false,
-    lazy: {
-      loadPrevNext: true,
-      loadOnTransitionStart: true,
-      loadPrevNextAmount: 2,
-    },
-    navigation: {
-      nextEl: '.swiper-next',
-      prevEl: '.swiper-prev',
-    },
-    breakpoints: {
-      640: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1280: {
-        slidesPerView: 2,
-        spaceBetween: 56,
-      },
-    },
-    on: {
-      init(swiper) {
-        swiper.$el.removeClass('opacity-0')
-        swiper.$el.addClass('opacity-100')
-      },
-    },
-  }
+  BaseCarouselCardsOptions
 )
 
 // BlockImageCarousel
+export const BlockImageCarouselOptions = {
+  slidesPerView: 1,
+  speed: 500,
+  threshold: 20, // swipe threshold in px
+  loop: false,
+  preloadImages: false,
+  watchSlidesProgress: true,
+  watchSlidesVisibility: true,
+  lazy: true,
+  navigation: {
+    nextEl: '.swiper-next',
+    prevEl: '.swiper-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+    bulletClass: 'swiper-dot',
+    bulletActiveClass: 'swiper-dot-active',
+    renderBullet: function (index, className) {
+      return `<button class="${className}" arial-label="Go to slide ${index}"><span></span></button>`
+    },
+  },
+}
+
 const BlockImageCarousel = new SwiperCore(
   '.BlockImageCarousel .swiper-container',
-  {
-    slidesPerView: 1,
-    speed: 500,
-    threshold: 20, // swipe threshold in px
-    loop: false,
-    preloadImages: false,
-    watchSlidesProgress: true,
-    watchSlidesVisibility: true,
-    lazy: true,
-    navigation: {
-      nextEl: '.swiper-next',
-      prevEl: '.swiper-prev',
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true,
-      bulletClass: 'swiper-dot',
-      bulletActiveClass: 'swiper-dot-active',
-      renderBullet: function (index, className) {
-        return `<button class="${className}" arial-label="Go to slide ${index}"><span></span></button>`
-      },
-    },
-  }
+  BlockImageCarouselOptions
 )
