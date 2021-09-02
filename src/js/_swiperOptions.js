6f40b08a5415b2e298bfd3eef28a7f38e2ec041a
Stephanie Smith <stephanie.c.smith@jpl.nasa.gov>
2021-09-02 11:36:26 -0700

    Restructuring swiper options export, bumping package version

--- src/js/_swiperOptions.js
+++ src/js/_swiperOptions.js
@@ -2,68 +2,69 @@
  * Can be imported for use in other swiper instances
  */
 
-// BaseCarouselCardsOptions
-export const BaseCarouselCardsOptions = {
-  slidesPerView: 1.25,
-  speed: 500,
-  threshold: 20, // swipe threshold in px
-  spaceBetween: 28,
-  watchOverflow: true,
-  watchSlidesProgress: true,
-  watchSlidesVisibility: true,
-  loop: false,
-  preloadImages: false,
-  lazy: {
-    loadPrevNext: true,
-    loadOnTransitionStart: true,
-    loadPrevNextAmount: 2,
-  },
-  navigation: {
-    nextEl: '.swiper-next',
-    prevEl: '.swiper-prev',
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 1,
+const swiperOptions = {
+  BaseCarouselCards: {
+    slidesPerView: 1.25,
+    speed: 500,
+    threshold: 20, // swipe threshold in px
+    spaceBetween: 28,
+    watchOverflow: true,
+    watchSlidesProgress: true,
+    watchSlidesVisibility: true,
+    loop: false,
+    preloadImages: false,
+    lazy: {
+      loadPrevNext: true,
+      loadOnTransitionStart: true,
+      loadPrevNextAmount: 2,
     },
-    768: {
-      slidesPerView: 2,
+    navigation: {
+      nextEl: '.swiper-next',
+      prevEl: '.swiper-prev',
     },
-    1280: {
-      slidesPerView: 2,
-      spaceBetween: 56,
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1280: {
+        slidesPerView: 2,
+        spaceBetween: 56,
+      },
+    },
+    on: {
+      init(swiper) {
+        swiper.$el.removeClass('opacity-0')
+        swiper.$el.addClass('opacity-100')
+      },
     },
   },
-  on: {
-    init(swiper) {
-      swiper.$el.removeClass('opacity-0')
-      swiper.$el.addClass('opacity-100')
+  BlockImageCarousel: {
+    slidesPerView: 1,
+    speed: 500,
+    threshold: 20, // swipe threshold in px
+    loop: false,
+    preloadImages: false,
+    watchSlidesProgress: true,
+    watchSlidesVisibility: true,
+    lazy: true,
+    navigation: {
+      nextEl: '.swiper-next',
+      prevEl: '.swiper-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+      bulletClass: 'swiper-dot',
+      bulletActiveClass: 'swiper-dot-active',
+      renderBullet: function (index, className) {
+        return `<button class="${className}" arial-label="Go to slide ${index}"><span></span></button>`
+      },
     },
   },
 }
 
-// BlockImageCarouselOptions
-export const BlockImageCarouselOptions = {
-  slidesPerView: 1,
-  speed: 500,
-  threshold: 20, // swipe threshold in px
-  loop: false,
-  preloadImages: false,
-  watchSlidesProgress: true,
-  watchSlidesVisibility: true,
-  lazy: true,
-  navigation: {
-    nextEl: '.swiper-next',
-    prevEl: '.swiper-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: true,
-    bulletClass: 'swiper-dot',
-    bulletActiveClass: 'swiper-dot-active',
-    renderBullet: function (index, className) {
-      return `<button class="${className}" arial-label="Go to slide ${index}"><span></span></button>`
-    },
-  },
-}
+export default swiperOptions
