f63c1074d938a7d6648fc13242075e410e0ca6e5
Luis Espinoza <laespino@jpl.nasa.gov>
2021-09-27 16:32:03 -0700

    making link_text an arg

--- storybook/stories/components/BaseImageCaption/BaseImageCaption.js
+++ storybook/stories/components/BaseImageCaption/BaseImageCaption.js
@@ -1,13 +1,13 @@
 import { BaseLinkTemplate } from '../BaseLink/BaseLink'
 
-export const BaseImageCaptionTemplate = ({ caption, credit, url }) => {
+export const BaseImageCaptionTemplate = ({ caption, credit, url, link_text }) => {
   if (credit) {
     credit = `<span class="inline"> Credit: ${credit} </span>`
   }
 
   let baseLink = BaseLinkTemplate({
     link: url,
-    text: 'Full Image Details', 
+    text: link_text, 
     variant: 'default',
     computedClass: '',
     wrapperClass: 'inline-block',
