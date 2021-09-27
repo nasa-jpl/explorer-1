8671cb5b683fb1d5ef0d72b7e3b91a609cef27d0
Luis Espinoza <laespino@jpl.nasa.gov>
2021-09-27 13:47:59 -0700

    adding `BasePlaceholder` component

---
+++ storybook/stories/components/BasePlaceholder/BasePlaceholder.stories.js
@@ -0,0 +1,16 @@
+import { BasePlaceholderTemplate } from './BasePlaceholder'
+
+export default {
+  title: 'Components/Base/BasePlaceholder',
+  parameters: {
+    viewMode: 'docs',
+    docs: {
+      description: {
+        component:
+          'An animated loading component frequently used as a placeholder for `SearchResultCard`',
+      },
+    },
+  },
+}
+
+export const Default = BasePlaceholderTemplate.bind()
