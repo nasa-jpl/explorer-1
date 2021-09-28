eb9c9bf98b77d68d16c0b97e41d4a30e9b653cde
Luis Espinoza <laespino@jpl.nasa.gov>
2021-09-27 17:10:20 -0700

    adding `BlockIframeEmbed` component

---
+++ storybook/stories/components/BlockIframeEmbed/BlockIframeEmbed.js
@@ -0,0 +1,31 @@
+import { BaseImageCaptionTemplate } from '../BaseImageCaption/BaseImageCaption'
+
+export const BlockIframeEmbedTemplate = ({title, url, height, caption, link_text}) => {
+  if(caption){
+    let baseImageCaption = BaseImageCaptionTemplate({
+      caption,
+      url,
+      link_text
+    })
+    caption = `<div class="lg:px-0 p-4 pb-0">${baseImageCaption}</div>`
+  }
+  
+  // height = height ? int(height) : 400
+
+  let template = `
+  <iframe
+    loading="lazy"
+    title="${title}"
+    data-src="${url}"
+    width="90%"
+    height="${height}"
+    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen="allowfullscreen"
+    class="BlockIframeEmbed"
+    src="${url}"
+  ></iframe>
+  ${caption}
+  `
+
+  return template
+}
