fc9a003a6f9176dc634703b9cb76e822b0709478
Luis Espinoza <laespino@jpl.nasa.gov>
2021-09-27 17:24:30 -0700

    Update BlockIframeEmbed.stories.js
    WIP

--- storybook/stories/components/BlockIframeEmbed/BlockIframeEmbed.stories.js
+++ storybook/stories/components/BlockIframeEmbed/BlockIframeEmbed.stories.js
@@ -43,4 +43,5 @@ Default.args = {
   url: 'https://eyes.nasa.gov/apps/orrery/#/sc_perseverance',
   height: 400,
   caption: '<p>With three giant blades stretching out some 66 feet (20 meters) from its cylindrical, six-sided body, the Juno spacecraft is a dynamic engineering marvel, spinning to keep itself stable as it makes oval-shaped orbits around Jupiter. View the full interactive experience at <a href="https://eyes.nasa.gov/apps/orrery/#/home">Eyes on the Solar System</a>.</p>',
+  link_text: '',
 }
