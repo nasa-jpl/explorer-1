e2982a35900c23407c15d49857e9736124ff4265
Luis Espinoza <laespino@jpl.nasa.gov>
2021-10-01 15:32:26 -0700

    adding 'BlockKeyPoints` component
    plus additional adjustments to other components based on new requirements

--- storybook/stories/components/BaseHeading/BaseHeading.js
+++ storybook/stories/components/BaseHeading/BaseHeading.js
@@ -1,3 +1,18 @@
-export const BaseHeadingTemplate = ({ text, size }) => {
-  return `<${size} class="text-${size}">${text}</${size}>`
+export const BaseHeadingTemplate = ({ text, size, tag, headingClass }) => {
+  let computedClass = headingClass ? headingClass + ' ' : ''
+  const tags = {
+    h1: 'text-h1',
+    h2: 'text-h2',
+    h3: 'text-h3',
+    h4: 'text-h4',
+    h5: 'text-h5',
+  }
+
+  if (tag) {
+    computedClass += tags[tag]
+  } else {
+    computedClass += tags[size]
+  }
+
+  return `<${size} class="${computedClass}">${text}</${size}>`
 }
