e9e38e07534cf5b840f754edd877b34033bce178
Luis Espinoza <laespino@jpl.nasa.gov>
2021-10-04 15:19:34 -0700

    adding fancybox

--- storybook/stories/components/BlockInlineImage/BlockInlineImage.js
+++ storybook/stories/components/BlockInlineImage/BlockInlineImage.js
@@ -1,8 +1,14 @@
 import { BlockTextTemplate } from '../BlockText/BlockText'
 import { BaseImagePlaceholderTemplate } from '../BaseImagePlaceholder/BaseImagePlaceholder'
 import { BaseImageCaptionTemplate } from '../BaseImageCaption/BaseImageCaption'
+import { BlockImageLightboxTemplate } from '../BlockImage/BlockImageLightbox'
 
-export const BlockInlineImageTemplate = ({ alignTo, text, image }) => {
+export const BlockInlineImageTemplate = ({
+  alignTo,
+  text,
+  image,
+  fancyboxGallery,
+}) => {
   let align =
     alignTo == 'right' ? 'col-start-8 lg:order-2' : 'col-start-1 lg:order-1'
   let textAlignTo =
@@ -11,19 +17,22 @@ export const BlockInlineImageTemplate = ({ alignTo, text, image }) => {
     { text }
   )}</div>`
 
-  // TODO: Add Fancybox lightbox option
-  let lightboxTemplate = ``
+  let blockImageLightbox = BlockImageLightboxTemplate({
+    image,
+    fancyboxGallery,
+  })
 
   let baseImagePlaceholder = BaseImagePlaceholderTemplate({
     theme: 'dark-theme',
-    template: lightboxTemplate,
     src: image.src.url,
     srcset: image.src.url,
     alt: image.alt,
     width: image.src.width,
     height: image.src.height,
-    objectFitClass: 'w-full h-auto object-contain',
+    wrapperClass: '',
+    objectFitClass: 'object-contain',
   })
+
   let baseImageCaption = BaseImageCaptionTemplate({
     caption: image.caption,
     credit: image.credit,
@@ -31,11 +40,15 @@ export const BlockInlineImageTemplate = ({ alignTo, text, image }) => {
     linkText: image.linkText,
     computedClass: ' lg:mt-3 mt-2',
   })
+
   let template = `
   <div class="BlockInlineImage lg:BaseGrid container mx-auto">
     <div class="lg:mb-0 lg:w-full sm:w-xl lg:mt-0 lg:pr-4 2xl:pr-0 col-span-5 mx-auto mt-8 mb-8 ${align}">
-      ${baseImagePlaceholder}
-      ${baseImageCaption}
+      <div class="group relative">
+        ${blockImageLightbox}
+        ${baseImagePlaceholder}
+        ${baseImageCaption}
+      </div>
     </div>
     ${blockText}
   </div>
