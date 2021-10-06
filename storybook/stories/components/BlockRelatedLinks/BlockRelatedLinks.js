845b999a2cca949b32f4a54a1d89386c5b9e20fa
Luis Espinoza <laespino@jpl.nasa.gov>
2021-10-05 18:42:52 -0700

    adding `BlockRelatedLinks` component + icons

---
+++ storybook/stories/components/BlockRelatedLinks/BlockRelatedLinks.js
@@ -0,0 +1,23 @@
+import { BaseHeadingTemplate } from '../BaseHeading/BaseHeading'
+import { RelatedLinkTemplate } from './RelatedLink'
+
+export const BlockRelatedLinksTemplate = ({ heading, links }) => {
+  let relatedLinks = ''
+  for (const [index, value] of links.entries()) {
+    relatedLinks += `<div class="sm:w-3/4 md:w-2/3 lg:w-3/4 xl:w-2/3 mb-2">${RelatedLinkTemplate(
+      { variant: value.variant, link: value.link, text: value.text }
+    )}</div>`
+  }
+
+  return `
+  <div>
+    ${BaseHeadingTemplate({
+      text: heading,
+      size: 'h5',
+      tag: 'h2',
+      headingClass: 'md:mb-8 mb-5',
+    })}
+    ${relatedLinks}
+  </div>
+  `
+}
