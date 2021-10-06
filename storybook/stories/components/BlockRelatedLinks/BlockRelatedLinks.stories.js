88bfa9950d3f4f8a0fb70e992b8c563153db4ed8
Luis Espinoza <laespino@jpl.nasa.gov>
2021-10-06 10:28:48 -0700

    add a single link story
    allows controls to be modified for single link, but can't change the header due to it being called in a decorator

--- storybook/stories/components/BlockRelatedLinks/BlockRelatedLinks.stories.js
+++ storybook/stories/components/BlockRelatedLinks/BlockRelatedLinks.stories.js
@@ -1,8 +1,43 @@
 import { BlockRelatedLinksTemplate } from './BlockRelatedLinks'
+import { RelatedLinkTemplate } from './RelatedLink'
 
 export default {
   title: 'Components/Blocks/BlockRelatedLinks',
   excludeStories: /.*Data$/,
+  argTypes: {
+    heading: {
+      type: { name: 'string', required: false },
+      description: 'A header for the list of links.',
+    },
+    text: {
+      type: { name: 'string', required: false },
+      description: 'Link text.',
+    },
+    variant: {
+      type: 'string',
+      description:
+        'Link to a document, internal, or external link.  The type of link chosen will determine the icon and target window behavior.',
+      control: {
+        type: 'select',
+        options: ['internal', 'external', 'document'],
+      },
+      table: {
+        defaultValue: { summary: 'external' },
+      },
+    },
+    link: {
+      type: { name: 'string', required: false },
+      description: '',
+    },
+  },
+  parameters: {
+    viewMode: 'docs',
+    docs: {
+      description: {
+        component: '',
+      },
+    },
+  },
 }
 
 export const BlockRelatedLinksData = {
@@ -31,8 +66,24 @@ export const BlockRelatedLinksData = {
   ],
 }
 
-export const BlockRelatedLinks = BlockRelatedLinksTemplate.bind({})
-BlockRelatedLinks.args = {
+export const SingleLink = RelatedLinkTemplate.bind({})
+SingleLink.args = {
+  heading: BlockRelatedLinksData.heading,
+  variant: BlockRelatedLinksData.links[0].variant,
+  link: BlockRelatedLinksData.links[0].link,
+  text: BlockRelatedLinksData.links[0].text,
+}
+SingleLink.decorators = [
+  (Story) => `
+  <div>
+    <h2 class="md:mb-8 mb-5 text-h5">${BlockRelatedLinksData.heading}</h2>
+    <div class="sm:w-3/4 md:w-2/3 lg:w-3/4 xl:w-2/3 mb-2">${Story()}</div>
+  </div>
+  `,
+]
+
+export const Default = BlockRelatedLinksTemplate.bind({})
+Default.args = {
   heading: BlockRelatedLinksData.heading,
   links: BlockRelatedLinksData.links,
 }
