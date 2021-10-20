916d70fb54e8fc60be3b68d18392c650632a141a
Luis Espinoza <laespino@jpl.nasa.gov>
2021-10-20 16:38:49 -0700

    WIP: adding `BlockImageCarousel` component
    working with the slider js plugin under `Canvas` view

---
+++ storybook/stories/components/BlockImageCarousel/BlockImageCarousel.js
@@ -0,0 +1,70 @@
+import { BlockImageGalleryCoverTemplate } from '../BlockImageGalleryCover/BlockImageGalleryCover'
+import { BlockImageStandardTemplate } from '../BlockImage/BlockImageStandard'
+import { BaseButtonTemplate } from '../BaseButton/BaseButton'
+
+export const BlockImageCarouselTemplate = ({
+  blockType,
+  images,
+  imageGallery,
+  fancyboxGallery,
+  label,
+  title,
+  description,
+}) => {
+  let slides = ''
+  if (images.length > 0) {
+    for (const [index, value] of images.entries()) {
+      slides += `<div class="swiper-slide">${BlockImageStandardTemplate({
+        image: value,
+        imageClass: 'swiper-lazy',
+        imageCaptionClass: 'slide-caption',
+        imageCaptionWrapperClass: 'lg:pt-3 px-4 pt-10',
+        fancyboxGallery,
+      })}</div>`
+    }
+  }
+
+  return `
+  <div class="BlockImageCarousel w-full overflow-hidden ${
+    blockType == 'image_gallery'
+      ? `BlockImageGallery max-w-screen-3xl mx-auto`
+      : ''
+  }">
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        ${
+          blockType == 'image_gallery'
+            ? BlockImageGalleryCoverTemplate({
+                label,
+                title,
+                description,
+                fancyboxGallery,
+                image: imageGallery,
+              })
+            : ''
+        }
+        ${slides}
+      </div>
+      <div class="swiper-nav lg:block absolute inset-x-0 top-0 hidden h-0 overflow-hidden">
+        <div class="absolute inset-0">
+          <div class="absolute bottom-0 right-0 z-10 flex">
+            ${BaseButtonTemplate({
+              variant: 'primary',
+              cssClass: 'z-10 border-collapse px-1 py-2 swiper-next xl:text-xl',
+              icon: 'prev',
+              label: 'Previous slide',
+            })}
+            ${BaseButtonTemplate({
+              variant: 'primary',
+              cssClass: 'z-10 border-collapse px-1 py-2 swiper-next xl:text-xl',
+              icon: 'next',
+              label: 'Next slide',
+            })}
+          </div>
+        </div>
+      </div>
+      <div class="swiper-pagination swiper-dots lg:hidden absolute top-0 w-full text-center"></div>
+    </div>
+  </div>
+  `
+}
