64e2260bc79bb0e7273f806e09e3f5fa286d11c7
Luis Espinoza <laespino@jpl.nasa.gov>
2021-10-21 15:49:18 -0700

    adding `BlockImageGallery` story

---
+++ storybook/stories/components/BlockImageGallery/BlockImageGallery.stories.js
@@ -0,0 +1,56 @@
+import { BlockImageCarouselData } from '../BlockImageCarousel/BlockImageCarousel.stories'
+import { BlockImageCarouselTemplate } from '../BlockImageCarousel/BlockImageCarousel'
+
+export default {
+  title: 'Components/Blocks/BlockImageGallery',
+  decorators: [
+    (Story) =>
+      `<div id="storyDecorator" class="max-w-screen-3xl mx-auto">${Story()}</div>`,
+  ],
+  argTypes: {
+    label: {
+      type: { name: 'string', required: false },
+      description: 'Cover label',
+    },
+    title: {
+      type: { name: 'string', required: true },
+      description: 'Cover title',
+    },
+    description: {
+      type: { name: 'string', required: false },
+      description: 'Cover summary/description',
+    },
+    images: {
+      type: { name: 'array', required: true },
+      description: 'An array of image data objects',
+    },
+  },
+  parameters: {
+    html: {
+      root: '#storyDecorator',
+    },
+    viewMode: 'canvas',
+    previewTabs: {
+      'storybook/docs/panel': {
+        hidden: true,
+      },
+    },
+  },
+}
+
+export const Default = BlockImageCarouselTemplate.bind({})
+Default.storyName = 'BlockImageGallery'
+Default.args = {
+  ...BlockImageCarouselData,
+  galleryCoverImage: {
+    label: 'Mars',
+    title: 'Image Gallery',
+    description:
+      'Donec dui nisl, maximus quis nibh eu, luctus ultrices nulla. Curabitur tempus, massa vitae porttitor porttitor, tortor erat tincidunt magna, vulputate varius nulla leo et mauris. Morbi blandit ac justo nec dictum. Nam pretium ex sit amet blandit malesuada.',
+    image: {
+      ...BlockImageCarouselData['images'][0],
+      caption: '',
+      credit: '',
+    },
+  },
+}
