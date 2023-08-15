7e4cff44bd3fc016ac75efdf1f3f839ff1ad503a
Albina <51043550+albinazs@users.noreply.github.com>
2023-08-15 15:22:07 +0300

    Add back to top component (#408)
    * Add back to top component
    
    * Add threshold and scrollto props to back-to-top
    
    * Add possibility to add multiple back-to-tops
    
    * Improve performance and clean code for back-to-top
    
    * Update naming and docs for back-to-top component
    
    * Accessibility improvements and clean code for back-to-top

---
+++ src/js/components/_BackToTop.js
@@ -0,0 +1,48 @@
+/*
+ **
+ ** Back To Top button for scrolling to the top
+ **
+ */
+
+const initBackToTop = (backToTop) => {
+  const backToTopBtn = backToTop.querySelector('button')
+  const threshold = parseFloat(backToTop.dataset.threshold) || 300
+  const scrollTo = parseFloat(backToTop.dataset.scrollto) || 0
+  const alwaysVisible = backToTop.dataset.alwaysvisible
+
+  const debounce = (func, wait = 0) => {
+    let timeoutId
+    return (...args) => {
+      clearTimeout(timeoutId)
+      timeoutId = setTimeout(() => {
+        func(...args)
+      }, wait)
+    }
+  }
+
+  const onScroll = () => {
+    if (!alwaysVisible) {
+      let showBackToTop = window.scrollY > threshold
+      showBackToTop
+        ? (backToTopBtn.style.display = 'block')
+        : (backToTopBtn.style.display = 'none')
+    }
+  }
+
+  const debouncedOnScroll = debounce(onScroll, 500)
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: scrollTo, behavior: 'smooth' })
+  }
+
+  if (!alwaysVisible) {
+    backToTopBtn.style.display = 'none'
+    window.addEventListener('scroll', debouncedOnScroll, { passive: true })
+    backToTopBtn.addEventListener('click', scrollToTop)
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const backToTopElements = document.querySelectorAll('.BackToTop')
+  backToTopElements.forEach(initBackToTop)
+})
