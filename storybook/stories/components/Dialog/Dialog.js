f96180c290af9ddd68e99e18887773046bb6898f
Albina Starykova <albina.starikova@gmail.com>
2023-08-16 15:54:49 +0200

    Clean code

--- storybook/stories/components/Dialog/Dialog.js
+++ storybook/stories/components/Dialog/Dialog.js
@@ -1,32 +1,26 @@
 import { IconCloseTemplate } from '../Icons/IconClose'
 
 export const DialogTemplate = ({
-  dialogId,
-  dialogTrigger,
+  dialogId = '',
+  dialogTrigger = '',
   ariaLabelledBy = '',
-  content,
+  content = '',
   overlayClose = 'true',
   dialogBoxClass = '',
 }) => {
   const template = `
     <div class="Dialog">
-        <span data-a11y-dialog-show="${dialogId}">${
-    dialogTrigger ? dialogTrigger : 'Dialog trigger'
-  }</span>  
+        <span data-a11y-dialog-show="${dialogId}">${dialogTrigger}</span>  
         <div id="${dialogId}"
-        aria-labelledby="${
-          ariaLabelledBy ? ariaLabelledBy : ''
-        }" aria-hidden='true'
+        aria-labelledby="${ariaLabelledBy}" aria-hidden='true'
         class="dialog-container fixed top-0 left-0 z-50 flex w-full h-full overflow-auto" >
             <div class="dialog-overlay ${
               overlayClose ? 'data-a11y-dialog-hide' : ''
             }
             bg-gray-dark bg-opacity-90 fixed top-0 left-0 w-full h-full"></div> 
             <div class="container m-auto">
-                <div role="document" class="dialog-box ${
-                  dialogBoxClass ? dialogBoxClass : ''
-                } relative m-auto bg-white">
-                    <button data-a11y-dialog-hide aria-label="Close dialog" 
+                <div role="document" class="dialog-box ${dialogBoxClass} relative m-auto bg-white">
+                    <button type="button" data-a11y-dialog-hide aria-label="Close dialog" 
                     class="button-close absolute right-0 flex-shrink-0 text-white focus:outline-none focus:ring-2">
                         ${IconCloseTemplate({})}
                     </button>           
