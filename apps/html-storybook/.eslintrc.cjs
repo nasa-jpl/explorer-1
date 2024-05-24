88784079ec4aa54807b2fded337a14afd8d87309
James Ray <james.a.ray@jpl.nasa.gov>
2024-05-24 03:40:02 -0700

    remove old eslint config files

--- apps/html-storybook/.eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-module.exports = {
-  root: true,
-  extends: ['eslint:recommended', 'plugin:storybook/recommended'],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
