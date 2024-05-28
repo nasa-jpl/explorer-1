ae6ecdd7a06d76b4e9508ca12d2db3184b4c02d7
Stephanie Smith <stephanie.c.smith@jpl.nasa.gov>
2024-05-28 10:10:30 -0700

    migrating to swiper 11

--- packages/html/src/assets/js/vendors/_swiper.js
+++ packages/html/src/assets/js/vendors/_swiper.js
@@ -2,20 +2,20 @@
  *  Configured to use any modules that are needed site-wide (SwiperCore.use([]))
  *  Custom options are defined in constants
  */
-
-import Swiper, { Navigation, Pagination, Lazy, A11y } from 'swiper'
+import Swiper from 'swiper'
+import { A11y, Navigation, Pagination } from 'swiper/modules'
 import swiperOptions from './_swiperOptions.js'
 
 export function initMixinCarousel() {
   return new Swiper('.MixinCarousel .swiper', {
-    modules: [Navigation, Lazy, A11y, Pagination],
+    modules: [A11y, Navigation, Pagination],
     ...swiperOptions.MixinCarousel
-  })
+  }).init()
 }
 
 export function initBlockImageCarousel() {
   return new Swiper('.BlockImageCarousel .swiper', {
-    modules: [Navigation, Lazy, A11y, Pagination],
+    modules: [A11y, Navigation, Pagination],
     ...swiperOptions.BlockImageCarousel
   })
 }
