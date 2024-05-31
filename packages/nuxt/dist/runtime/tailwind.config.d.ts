bc7626f5a6b9031d0971fa81074f66ed31453e88
Stephanie Smith <stephiescastle@users.noreply.github.com>
2024-05-30 17:32:05 -0700

    fixing nuxt module build error (#452)
    * using createResolver
    
    * fixing issue with importing vite config from @explorer-1/vue

---
+++ packages/nuxt/dist/runtime/tailwind.config.d.ts
@@ -0,0 +1,12 @@
+declare const _default: {
+    content: string[];
+    mode: string;
+    darkMode: "class";
+    theme: Partial<import("../../../common/node_modules/tailwindcss/types/config").CustomThemeConfig>;
+    plugins: any[];
+    future: {
+        hoverOnlyWhenSupported: true;
+    };
+    safelist: string[];
+};
+export default _default;
