06a31ef87a84889e1c882b0ba5f7065f379d846f
stephiescastle <stephiescastle@users.noreply.github.com>
2024-06-12 20:35:09 +0000

    Deploying to gh-pages from @ nasa-jpl/explorer-1@bef830e9baea001bce55890bf70f01b300de66f6 🚀

---
+++ assets/accessibility.docs-CYKbxLje.js
@@ -0,0 +1,29 @@
+import{j as e}from"./jsx-runtime-BpKW7tLe.js";import{u as i}from"./index-hvrDetzx.js";import{M as s}from"./index-DfR7D5b7.js";import"./iframe-Cshp5o3p.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./chunk-QN4WKJDJ-Bf_F3oir.js";import"./doctrine-SJNQURFN.js";function t(o){const n={a:"a",blockquote:"blockquote",h1:"h1",h2:"h2",h3:"h3",li:"li",ol:"ol",p:"p",ul:"ul",...i(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"Guides/Accessibility",parameters:{viewMode:"docs"}}),`
+`,e.jsx(n.h1,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(n.p,{children:["Usage of Explorer 1 must be in compliance with ",e.jsx(n.a,{href:"https://www.access-board.gov/guidelines-and-standards/communications-and-it/about-the-ict-refresh/final-rule/text-of-the-standards-and-guidelines",rel:"nofollow",children:"Section 508 of the Rehabilitation Act of 2001"})," which states that:"]}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["Electronic content shall conform to Level A and Level AA Success Criteria and Conformance Requirements in ",e.jsx(n.a,{href:"https://www.w3.org/TR/WCAG20/",rel:"nofollow",children:"WCAG 2.0"})]}),`
+`]}),`
+`,e.jsx(n.h2,{id:"our-qa-strategy",children:"Our QA Strategy"}),`
+`,e.jsx(n.p,{children:"To meet compliance requirements, we use the following strategy:"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"When developing a component, employ best practices to make individual components accessible."}),`
+`,e.jsxs(n.li,{children:["Follow up with ",e.jsx(n.a,{href:"#manual-testing",children:"manual testing"})," of the component."]}),`
+`,e.jsx(n.li,{children:"Use automated accessibility tests as part of our CI/CD process for any final checks."}),`
+`,e.jsx(n.li,{children:"Finally, run checks (manual and automated) on select full pages."}),`
+`]}),`
+`,e.jsx(n.h3,{id:"automated-testing-options",children:"Automated testing options"}),`
+`,e.jsxs(n.p,{children:["Automated tests are a good baseline, but note they can only find ",e.jsx(n.a,{href:"https://alphagov.github.io/accessibility-tool-audit/",rel:"nofollow",children:"30 to 40% of issues"}),"."]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["For individual components, the Explorer 1 Storybook uses the ",e.jsx(n.a,{href:"https://github.com/storybookjs/storybook/tree/master/addons/a11y",rel:"nofollow",children:"a11y addon"}),", and in some cases ",e.jsx(n.a,{href:"https://www.deque.com/axe/",rel:"nofollow",children:"Axe"})," in jsdom via Storyshots"]}),`
+`,e.jsxs(n.li,{children:["For full pages, ",e.jsx(n.a,{href:"https://github.com/GoogleChrome/lighthouse-ci",rel:"nofollow",children:"Lighthouse CI"})," checks are recommended."]}),`
+`]}),`
+`,e.jsx("span",{id:"manual-testing"}),`
+`,e.jsx(n.h3,{id:"manual-testing",children:"Manual testing"}),`
+`,e.jsx(n.p,{children:"When conducting manual accessibility testing, check for the following as a baseline:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Keyboard-only navigation is possible"}),`
+`,e.jsx(n.li,{children:"Screen reader announces the UI as expected"}),`
+`,e.jsx(n.li,{children:"Mobile touch interaction testing with iOS Safari or Android"}),`
+`,e.jsx(n.li,{children:"Zoom the whole browser UI, or font size, or on-screen magnifier"}),`
+`]})]})}function p(o={}){const{wrapper:n}={...i(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(t,{...o})}):t(o)}export{p as default};
---
+++ assets/responsive.docs-DIug0tGb.js
@@ -0,0 +1,35 @@
+import{j as e}from"./jsx-runtime-BpKW7tLe.js";import{u as i}from"./index-hvrDetzx.js";import{M as r,C as d}from"./index-DfR7D5b7.js";import{S as l,D as o}from"./responsive.stories-BpOfwm-d.js";import"./iframe-Cshp5o3p.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./chunk-QN4WKJDJ-Bf_F3oir.js";import"./doctrine-SJNQURFN.js";function s(t){const n={a:"a",blockquote:"blockquote",code:"code",h1:"h1",h2:"h2",li:"li",p:"p",pre:"pre",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...i(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:l}),`
+`,e.jsx(n.h1,{id:"responsive-design",children:"Responsive Design"}),`
+`,e.jsx(n.p,{children:"Responsive design adapts the flow and layout of information to respond to a user’s behavior and environment based on screen size, platform and orientation. Adapting the flow of information and visual elements may sometimes require an optimized user experience to help fit content within certain parameters of a particular display or device. It may also require limiting the layout of the design to only include necessary modules, depending on a device’s parameters and resolution."}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"#breakpoints-and-containers",children:"Breakpoints and Containers"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"#maximum-width",children:"Maximum Width"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"#tokens",children:"Tokens"})}),`
+`]}),`
+`,e.jsx("span",{id:"breakpoints-and-containers"}),`
+`,e.jsx(n.h2,{id:"breakpoints-and-containers",children:"Breakpoints and Containers"}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsx(n.p,{children:"The examples on this page illustrate breakpoints based on viewport size. Try changing your browser size to see how the examples adapt. You can also switch over to canvas view and toggle through different viewport sizes using the toolbar menu."}),`
+`]}),`
+`,e.jsx(n.p,{children:"Breakpoints are the values at which content included on a webpage is resized and reflowed to fit a screen size on a particular device. Designing for different breakpoints allows designers and developers to optimize the display of information for mobile, tablet and desktop users alike."}),`
+`,e.jsxs(n.p,{children:["We set our breakpoints and associated containers via the ",e.jsx(n.code,{children:"screens"})," and ",e.jsx(n.code,{children:"containers.screens"})," options in our ",e.jsx(n.a,{href:"?path=/docs/appendix-tailwindcss--docs",children:"Tailwind CSS configuration"}),". These containers are used in conjunction with our ",e.jsx(n.a,{href:"?path=/docs/foundation-grid-and-layout--docs",children:"Grid"})," to create the site and page layouts, all the way down to the component level."]}),`
+`,e.jsxs(n.table,{children:[e.jsx(n.thead,{children:e.jsxs(n.tr,{children:[e.jsx(n.th,{style:{textAlign:"left"},children:"Screen Token"}),e.jsxs(n.th,{style:{textAlign:"right"},children:["Viewport ",e.jsx(n.code,{children:"min-width"})]}),e.jsxs(n.th,{style:{textAlign:"right"},children:[e.jsx(n.code,{children:".container"})," ",e.jsx(n.code,{children:"max-width"})]})]})}),e.jsxs(n.tbody,{children:[e.jsxs(n.tr,{children:[e.jsx(n.td,{style:{textAlign:"left"},children:e.jsx(n.code,{children:"sm"})}),e.jsx(n.td,{style:{textAlign:"right"},children:e.jsx(n.code,{children:"640px"})}),e.jsx(n.td,{style:{textAlign:"right"},children:e.jsx(n.code,{children:"640px"})})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{style:{textAlign:"left"},children:e.jsx(n.code,{children:"md"})}),e.jsx(n.td,{style:{textAlign:"right"},children:e.jsx(n.code,{children:"768px"})}),e.jsx(n.td,{style:{textAlign:"right"},children:e.jsx(n.code,{children:"768px"})})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{style:{textAlign:"left"},children:e.jsx(n.code,{children:"lg"})}),e.jsx(n.td,{style:{textAlign:"right"},children:e.jsx(n.code,{children:"1024px"})}),e.jsx(n.td,{style:{textAlign:"right"},children:e.jsx(n.code,{children:"1024px"})})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{style:{textAlign:"left"},children:e.jsx(n.code,{children:"xl"})}),e.jsx(n.td,{style:{textAlign:"right"},children:e.jsx(n.code,{children:"1280px"})}),e.jsx(n.td,{style:{textAlign:"right"},children:e.jsx(n.code,{children:"1280px"})})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{style:{textAlign:"left"},children:e.jsx(n.code,{children:"2xl"})}),e.jsx(n.td,{style:{textAlign:"right"},children:e.jsx(n.code,{children:"1440px"})}),e.jsx(n.td,{style:{textAlign:"right"},children:e.jsx(n.code,{children:"1320px"})})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{style:{textAlign:"left"},children:e.jsx(n.code,{children:"3xl"})}),e.jsx(n.td,{style:{textAlign:"right"},children:e.jsx(n.code,{children:"1800px"})}),e.jsx(n.td,{style:{textAlign:"right"},children:e.jsx(n.code,{children:"1320px"})})]})]})]}),`
+`,e.jsxs(n.p,{children:["The below example uses ",e.jsx(n.a,{href:"#tokens",children:"Tokens"})," to set the maximum width based on the screens defined in our Tailwind config. It also illustrates the foundation of the design system's layout for desktop screens."]}),`
+`,e.jsx(d,{className:"sbdocs-preview-full-width",of:o}),`
+`,e.jsx("span",{id:"maximum-width"}),`
+`,e.jsx(n.h2,{id:"maximum-width",children:"Maximum width"}),`
+`,e.jsxs(n.p,{children:["Explorer 1 uses screen ",e.jsx(n.code,{children:"3xl"}),' as its maximum width. No content in a component, not even in those labeled as "full bleed," ever exceeds ',e.jsx(n.code,{children:"3xl"}),", although visual layout elements may (i.e. background colors). You can find examples of this in our ",e.jsx(n.a,{href:"?path=/docs/foundation-grid-and-layout--docs#page-layout-recommendations",children:"Grid & Layout"})," documentation."]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-html",children:`<div class="max-w-screen-3xl mx-auto">Content</div>
+`})}),`
+`,e.jsx("span",{id:"tokens"}),`
+`,e.jsx(n.h2,{id:"tokens",children:"Tokens"}),`
+`,e.jsx(n.p,{children:"Screen tokens are used with Tailwind CSS classes to control widths:"}),`
+`,e.jsx(n.p,{children:e.jsx(n.code,{children:".max-w-screen-{token}"})}),`
+`,e.jsx(n.p,{children:"They can also be used as directives for writing media queries:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-css",children:`@screen lg {
+  /* rules for large screens */
+}
+@screen xl {
+  /* rules for xl screens */
+}
+`})})]})}function f(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(s,{...t})}):s(t)}export{f as default};
