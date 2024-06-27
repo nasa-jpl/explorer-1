1f9e0437e73effe7537409ac53d20f47b20151f6
Stephanie Smith <stephiescastle@users.noreply.github.com>
2024-06-26 17:31:00 -0700

    Fixes for pnpm publish (#472)
    * updating package.json
    
    * updating privacy on packages
    
    * removing dist folders
    
    * trying with ts-check removed from eslint index
    
    * fixing dist in gitignore

--- packages/vue/dist/src/components/SkipLink/SkipLink.stories.d.ts
+++
@@ -1,8 +0,0 @@
-import { default as SkipLink } from './SkipLink.vue';
-declare namespace _default {
-    export let title: string;
-    export { SkipLink as component };
-    export let excludeStories: RegExp;
-}
-export default _default;
-export const Default: {};
