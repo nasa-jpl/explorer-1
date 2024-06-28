7517743f285fa4637a934c5f14de293bc1372627
Stephanie Smith <stephiescastle@users.noreply.github.com>
2024-06-28 12:50:04 -0700

    adjusting dependencies for explorer-1/common (#478)
    * adjusting dependencies for explorer-1/common
    
    * adding file type endings to dayjs imports
    
    * bumping version number for @explorer-1/vue
    
    * Update eslint.config.js
    
    ---------
    
    Co-authored-by: James Ray <james.a.ray@jpl.nasa.gov>

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 // eslint.config.js
+// imports config from the shared eslint-config package `packages/configs/eslint`
 import config from '@explorer-1/eslint-config'
 
 export default [
--- packages/vue/src/utils/dayjs.js
+++ packages/vue/src/utils/dayjs.js
@@ -1,12 +1,12 @@
 import dayjs from 'dayjs'
-import updateLocale from 'dayjs/plugin/updateLocale'
-import localizedFormat from 'dayjs/plugin/localizedFormat'
-import timezone from 'dayjs/plugin/timezone'
-import advancedFormat from 'dayjs/plugin/advancedFormat'
+import updateLocale from 'dayjs/plugin/updateLocale.js'
+import localizedFormat from 'dayjs/plugin/localizedFormat.js'
+import timezone from 'dayjs/plugin/timezone.js'
+import advancedFormat from 'dayjs/plugin/advancedFormat.js'
 
 // Locales must be imported manually
 // see https://github.com/iamkun/dayjs/tree/dev/src/locale
-import 'dayjs/locale/en-gb'
+import 'dayjs/locale/en-gb.js'
 
 dayjs.extend(localizedFormat)
 dayjs.extend(updateLocale)
