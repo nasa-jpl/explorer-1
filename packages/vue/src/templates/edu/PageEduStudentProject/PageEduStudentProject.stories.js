f0a1ad536f31a59dd44c16dd90230d242f6ca231
Stephanie Smith <stephanie.c.smith@jpl.nasa.gov>
2024-09-19 17:59:13 -0700

    resolving remaining conflict

--- packages/vue/src/templates/edu/PageEduStudentProject/PageEduStudentProject.stories.js
+++ packages/vue/src/templates/edu/PageEduStudentProject/PageEduStudentProject.stories.js
@@ -9,11 +9,8 @@ import { BaseVideoData } from './../../../components/BaseVideo/BaseVideo.stories
 import { BlockVideoEmbedData } from './../../../components/BlockVideoEmbed/BlockVideoEmbed.stories'
 import { BlockRelatedLinksData } from './../../../components/BlockRelatedLinks/BlockRelatedLinks.stories.js'
 import { BlockLinkCardCarouselData } from './../../../components/BlockLinkCarousel/BlockLinkCarousel.stories.js'
-<<<<<<< HEAD
 import { RichTextMediaData } from './../../../components/BlockText/BlockText.stories.js'
-=======
 import { BlockRichTableData } from './../../../components/BlockRichTable/BlockRichTable.stories'
->>>>>>> main
 import {
   BlockStreamfieldTruncatedData,
   BlockStreamfieldMinimalData
