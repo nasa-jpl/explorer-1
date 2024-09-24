59a82922dea96d161e5699600fcda7f2500e0012
Stephanie Smith <stephiescastle@users.noreply.github.com>
2024-09-24 11:20:12 -0700

    fix: EDU BlockRichTable adjustments (#633)
    * changing edu table header color
    
    * adjusting min width settings for blocks inside BlockRichTable

--- packages/vue/src/components/BlockRichTable/BlockRichTable.stories.js
+++ packages/vue/src/components/BlockRichTable/BlockRichTable.stories.js
@@ -72,3 +72,97 @@ export const BaseStory = {
     table: BlockRichTableData
   }
 }
+export const MixedColumnWidths = {
+  args: {
+    table: {
+      blockType: 'RichTableBlock',
+      tableCaption:
+        'Ut dapibus cursus quam, non dapibus diam pellentesque ac. Maecenas ultrices porta dui eget placerat. Curabitur ornare congue interdum.',
+      tableContent: {
+        tableHead: [
+          [
+            {
+              text: 'Image'
+            },
+            {
+              text: 'Type'
+            },
+            {
+              text: 'Description'
+            }
+          ]
+        ],
+        tableBody: [
+          [
+            {
+              ...BlockImageData,
+              caption: '<p>Custom ImageCaption</p>',
+              displayCaption: true,
+              blockType: 'ImageBlock'
+            },
+            {
+              blockType: 'CharBlock',
+              value: 'Internal'
+            },
+            {
+              blockType: 'RichTextBlock',
+              value:
+                '<p>Lorem ipsum <a href="/missions/test-mission/">dolor</a> sit amet, consectetur adipiscing elit. Quisque vitae justo quis justo malesuada molestie. Cras sed tincidunt dui.</p>\n'
+            }
+          ],
+          [
+            {
+              ...BlockImageData,
+              caption: '<p>Custom ImageCaption</p>',
+              displayCaption: true,
+              blockType: 'ImageBlock'
+            },
+            {
+              blockType: 'CharBlock',
+              value: 'External'
+            },
+            {
+              blockType: 'RichTextBlock',
+              value:
+                '<p>Morbi pretium, massa non convallis facilisis, lectus eros vulputate turpis, et imperdiet eros metus eu enim. Cras consequat iaculis leo eget auctor. Sed bibendum, nulla vel ultricies aliquam, augue mauris sagittis massa, nec malesuada massa justo id sem. In hac habitasse platea dictumst. Sed ullamcorper bibendum libero vitae pellentesque.</p>\n'
+            }
+          ],
+          [
+            {
+              ...BlockImageData,
+              caption: '<p>Custom ImageCaption</p>',
+              displayCaption: false,
+              blockType: 'ImageBlock'
+            },
+            {
+              blockType: 'CharBlock',
+              value: 'N/A'
+            },
+            {
+              blockType: 'RichTextBlock',
+              value:
+                '<p>Maecenas vel dapibus ligula, pretium <a href="#">dictum est</a>. Proin venenatis massa vulputate <strong>est rhoncus</strong>, sed ornare ex sagittis. Donec iaculis magna in rhoncus malesuada.</p>\n'
+            }
+          ],
+          [
+            {
+              ...BlockImageData,
+              caption: '<p>Custom ImageCaption</p>',
+              displayCaption: true,
+              blockType: 'ImageBlock'
+            },
+            {
+              blockType: 'CharBlock',
+              value: 'Internal'
+            },
+            {
+              blockType: 'RichTextBlock',
+              value:
+                '<p>Lorem ipsum <a href="/missions/test-mission/">dolor</a> sit amet, consectetur adipiscing elit. Quisque vitae justo quis justo malesuada molestie. Cras sed tincidunt dui.</p>\n'
+            }
+          ]
+        ]
+      }
+    }
+  }
+}
