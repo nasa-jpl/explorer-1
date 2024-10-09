c7f91512b26003f64d89c69894540ce40704fb10
Stephanie Smith <stephiescastle@users.noreply.github.com>
2024-10-09 08:35:42 -0700

    feature: edu subject icons (#654)
    * Adding edu subject icons to vue package
    
    * using subject icons in Meta data icons for edu subjects
    
    * fixing build error

---
+++ packages/vue/src/components/Icons/IconEduEnglishLanguageArts.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg
+    class="IconEduEnglishLanguageArts"
+    width="24"
+    height="25"
+    viewBox="0 0 24 25"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+    focusable="false"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M7.5 4.51172C6.39543 4.51172 5.5 5.40715 5.5 6.51172V15.8368C5.96046 15.6159 6.4764 15.4922 7.02124 15.4922H18.4922V4.51172H7.5ZM3.5 18.8392H3.50423C3.50142 18.8969 3.5 18.955 3.5 19.0134C3.5 20.9582 5.07651 22.5347 7.02124 22.5347H20.5V20.5347H19.4316V17.4922H20.5V15.4922H20.4922V2.51172H7.5C5.29086 2.51172 3.5 4.30258 3.5 6.51172V18.8392ZM17.4316 17.4922H7.02124C6.18108 17.4922 5.5 18.1733 5.5 19.0134C5.5 19.8536 6.18108 20.5347 7.02124 20.5347H17.4316V17.4922ZM12.8675 10.9254L12.2411 9.52195L11.6146 10.9254H12.8675ZM13.5817 12.5254H10.9003L10.2188 14.0518L8.75781 13.3996L12.2413 5.59668L15.724 13.3998L14.263 14.0519L13.5817 12.5254Z"
+      fill="currentColor"
+    />
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'IconEduEnglishLanguageArts'
+})
+</script>
+<style lang="scss">
+@import '@explorer-1/common/src/scss/components/IconEduEnglishLanguageArts';
+</style>
