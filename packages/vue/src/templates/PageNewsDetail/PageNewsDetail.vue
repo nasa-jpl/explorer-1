a92877b70ac26b3bb545bd2687952f4080a2327e
Stephanie Smith <stephiescastle@users.noreply.github.com>
2024-12-06 10:46:57 -0500

    fix: allowing hero videos to be inline on news pages (#712)
    * allowing hero videos to be inline on news pages
    
    * switching logic from show to hideShareLinks
    
    * Revert "switching logic from show to hideShareLinks"
    
    This reverts commit 2f362a524f1e2986c4ddb4d8b8e4db45c6d8be29.
    
    * fixing BlockVideo import, adding autplay property to video data

--- packages/vue/src/templates/PageNewsDetail/PageNewsDetail.vue
+++ packages/vue/src/templates/PageNewsDetail/PageNewsDetail.vue
@@ -176,7 +176,7 @@ import BlockImageStandard from './../../components/BlockImage/BlockImageStandard
 import BlockLinkCarousel from './../../components/BlockLinkCarousel/BlockLinkCarousel.vue'
 import ShareButtons from './../../components/ShareButtons/ShareButtons.vue'
 import BlockText from './../../components/BlockText/BlockText.vue'
-import BlockVideo from './../../components/BlockText/BlockText.vue'
+import BlockVideo from './../../components/BlockVideo/BlockVideo.vue'
 
 export default defineComponent({
   name: 'PageNewsDetail',
@@ -207,16 +207,13 @@ export default defineComponent({
       return (this.data?.hero || []).length === 0
     },
     heroInline(): boolean {
-      if (!this.heroEmpty) {
-        if (this.data?.hero[0].blockType === 'VideoBlock') {
-          return false
-        } else if (
-          this.data?.heroPosition === 'inline' ||
+      if (
+        !this.heroEmpty &&
+        (this.data?.heroPosition === 'inline' ||
           this.data?.hero[0].blockType === 'CarouselBlock' ||
-          this.data?.hero[0].blockType === 'VideoEmbedBlock'
-        ) {
-          return true
-        }
+          this.data?.hero[0].blockType === 'VideoEmbedBlock')
+      ) {
+        return true
       }
       return false
     },
