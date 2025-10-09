6b7a1c9ac0ea256bf26a6aa339bf87b6682cdaac
Stephanie Smith <stephanie.c.smith@jpl.nasa.gov>
2025-10-09 09:23:27 -0700

    removing commented out code

--- packages/html/src/main.js
+++ packages/html/src/main.js
@@ -1,22 +1,3 @@
-// import '@fancyapps/ui'
-// import 'lazysizes'
-// import {
-//   initMixinCarousel,
-//   initBlockImageCarousel
-// } from '@explorer-1/common/src/js/vendors/_swiper.js'
-// import initBlockAccordion from '@explorer-1/common/src/js/components/_BlockAccordion.js'
-// import initBackToTop from '@explorer-1/common/src/js/components/_BackToTop.js'
-// import initHeaderInternal from '@explorer-1/common/src/js/components/_HeaderInternal.js'
-// import initHeroMedia from '@explorer-1/common/src/js/components/_HeroMedia.js'
-// import './assets/scss/styles.scss'
-
-// initBackToTop()
-// initBlockAccordion()
-// initHeaderInternal()
-// initHeroMedia()
-// initMixinCarousel()
-// initBlockImageCarousel()
-
 // Vite Entrypoint
 
 // JavaScript
