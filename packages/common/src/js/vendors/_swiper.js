1f33df0d34454e71f1a1aa7b9c8eb3e29f4351e5
Stephanie Smith <stephanie.c.smith@jpl.nasa.gov>
2025-10-09 15:06:43 -0700

    fixing swiper in vanilla js, removing lazysizes

--- packages/common/src/js/vendors/_swiper.js
+++ packages/common/src/js/vendors/_swiper.js
@@ -1,21 +1,25 @@
 /* Swiper
- *  Configured to use any modules that are needed site-wide (SwiperCore.use([]))
- *  Custom options are defined in constants
+ * Configured to use any modules that are needed site-wide (Swiper.use([]))
+ * Custom options are defined in constants
  */
 import Swiper from 'swiper'
 import { A11y, Navigation, Pagination } from 'swiper/modules'
-import swiperOptions from './_swiperOptions.js'
+import swiperOptions from '@explorer-1/common/src/js/vendors/_swiperOptions.js'
+
+// 1. REGISTER MODULES GLOBALLY
+// Swiper 9+ requires modules to be registered using Swiper.use()
+Swiper.use([A11y, Navigation, Pagination])
 
 export function initMixinCarousel() {
+  // 2. Remove 'modules' option (they are registered globally) and the redundant .init() call
   return new Swiper('.MixinCarousel .swiper', {
-    modules: [A11y, Navigation, Pagination],
     ...swiperOptions.MixinCarousel
   }).init()
 }
 
 export function initBlockImageCarousel() {
+  // 3. Remove 'modules' option (they are registered globally)
   return new Swiper('.BlockImageCarousel .swiper', {
-    modules: [A11y, Navigation, Pagination],
     ...swiperOptions.BlockImageCarousel
   })
 }
