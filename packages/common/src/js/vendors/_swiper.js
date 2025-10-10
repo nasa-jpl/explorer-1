b5c84c665d60190652e24166db9a03edd3c3339e
Stephanie Smith <stephanie.c.smith@jpl.nasa.gov>
2025-10-09 18:42:45 -0700

    updating comment

--- packages/common/src/js/vendors/_swiper.js
+++ packages/common/src/js/vendors/_swiper.js
@@ -6,19 +6,16 @@ import Swiper from 'swiper'
 import { A11y, Navigation, Pagination } from 'swiper/modules'
 import swiperOptions from '@explorer-1/common/src/js/vendors/_swiperOptions.js'
 
-// 1. REGISTER MODULES GLOBALLY
-// Swiper 9+ requires modules to be registered using Swiper.use()
+// Register modules globally
 Swiper.use([A11y, Navigation, Pagination])
 
 export function initMixinCarousel() {
-  // 2. Remove 'modules' option (they are registered globally) and the redundant .init() call
   return new Swiper('.MixinCarousel .swiper', {
     ...swiperOptions.MixinCarousel
   })
 }
 
 export function initBlockImageCarousel() {
-  // 3. Remove 'modules' option (they are registered globally)
   return new Swiper('.BlockImageCarousel .swiper', {
     ...swiperOptions.BlockImageCarousel
   })
