33ff2c2dd05a142f0aa964478657ec508ada25ae
Steven T Fimbres <Steven.T.Fimbres@jpl.nasa.gov>
2025-11-25 11:08:59 -0800

    Add Icon Bookmark

---
+++ packages/vue/src/components/Icons/IconBookmark.vue
@@ -0,0 +1,14 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" class="IconBookmark" width="16" height="21" viewBox="0 0 16 21" fill="none">
+        <path d="M0 21L8 16.4062L16 21V0H0V21Z" fill="#949494"/>
+    </svg>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'IconHourglass'
+})
+</script>
+<style lang="scss">
+@import '@explorer-1/common/src/scss/components/IconHourglass';
+</style>
