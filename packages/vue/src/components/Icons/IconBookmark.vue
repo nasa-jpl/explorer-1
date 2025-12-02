8cb075c7c163306d9df0fa3b6d43fa93cb2fb71d
Steven T Fimbres <Steven.T.Fimbres@jpl.nasa.gov>
2025-12-02 09:49:47 -0800

    Linting Fix

--- packages/vue/src/components/Icons/IconBookmark.vue
+++ packages/vue/src/components/Icons/IconBookmark.vue
@@ -1,7 +1,18 @@
 <template>
-    <svg xmlns="http://www.w3.org/2000/svg" class="IconBookmark" width="16" height="21" viewBox="0 0 16 21" fill="none">
-        <path d="M0 21L8 16.4062L16 21V0H0V21Z" fill="#949494"/>
-    </svg>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="IconBookmark"
+    width="16"
+    height="21"
+    viewBox="0 0 16 21"
+    fill="none"
+  >
+    >
+    <path
+      d="M0 21L8 16.4062L16 21V0H0V21Z"
+      fill="#949494"
+    />
+  </svg>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
--- packages/vue/src/components/MetadataStacked/MetadataStacked.vue
+++ packages/vue/src/components/MetadataStacked/MetadataStacked.vue
@@ -19,16 +19,30 @@ const props = withDefaults(defineProps<MetadataStackedProps>(), {
 <template>
   <div class="MetadataStacked inline-flex flex-col flex-start gap-2.5 text-body-md">
     <div class="flex flex-row gap-2.5 items-center self-stretch">
-      <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
-        <circle cx="14" cy="14" r="14" fill="#7C6500"/>
-      </svg><!--TODO | Add logic to update | Should this be a prop?-->
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="28"
+        height="28"
+        viewBox="0 0 28 28"
+        fill="none"
+      >
+        <circle
+          cx="14"
+          cy="14"
+          r="14"
+          fill="#7C6500"
+        />
+      </svg>
       <span>{{ props.location }}</span>
     </div>
     <div class="flex flex-row gap-2.5 items-center self-stretch">
-      <IconHourglass class="MetadataStackedIcon text-jpl-red"/>
+      <IconHourglass class="MetadataStackedIcon text-jpl-red" />
       <span>{{ props.wait }}</span>
     </div>
-    <div class="flex flex-row gap-2.5 items-center self-stretch" v-if="props.showMyList">
+    <div
+      v-if="props.showMyList"
+      class="flex flex-row gap-2.5 items-center self-stretch"
+    >
       <IconBookmark class="MetadataStackedIcon text-gray-mid" />
       <span class="italic">Added to my list</span>
     </div>
