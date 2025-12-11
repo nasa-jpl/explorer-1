d2bf0b8926b15ae5fe42b25695a1ff7fee965564
Stephanie Smith <stephanie.c.smith@jpl.nasa.gov>
2025-12-11 11:42:22 -0800

    fixing path of artifact

--- apps/vue-storybook/.storybook/preview.ts
+++ apps/vue-storybook/.storybook/preview.ts
@@ -20,16 +20,11 @@ import { beautifyHtmlOptions } from '@explorer-1/common-storybook/src/plugins/be
 // --- Gracefully Handle Licensed Fonts ---
 const IS_CHROMATIC_BUILD = import.meta.env.VITE_CHROMATIC_BUILD === 'true'
 // artifact placed in root of repo
-const LICENSED_FONTS_PATH = './../../licensed-fonts-base64.css'
+const LICENSED_FONTS_PATH = '.licensed-fonts-base64.css'
 // --- Conditional Import of Licensed Fonts ---
 if (IS_CHROMATIC_BUILD) {
   try {
-    if (typeof require !== 'undefined') {
-      require(LICENSED_FONTS_PATH)
-    } else {
-      // Fallback for strict ESM environments (less likely in Storybook setup)
-      import(LICENSED_FONTS_PATH)
-    }
+    import(/* @vite-ignore */ LICENSED_FONTS_PATH)
     console.log('✅ Licensed Fonts loaded.')
   } catch (error) {
     console.error('❌ FATAL ERROR: Licensed Fonts file not found!')
