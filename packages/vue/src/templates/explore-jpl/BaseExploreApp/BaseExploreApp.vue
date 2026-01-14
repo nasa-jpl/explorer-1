ea48662ebc652de1fa920fb9670c75c254084dab
Steven Fimbres <32422862+sfimbres@users.noreply.github.com>
2026-01-14 14:55:33 -0800

    Feature: Update Explore App Templates (#803)
    * Update prop pageDate to data
    
    * Setup placeholder template for Info page
    
    * Comment out unused imports
    
    * removing hard-coded fallback nav for the Explore App
    
    * Fix merging error
    
    ---------
    
    Co-authored-by: Stephanie Smith <stephanie.c.smith@jpl.nasa.gov>

--- packages/vue/src/templates/explore-jpl/BaseExploreApp/BaseExploreApp.vue
+++ packages/vue/src/templates/explore-jpl/BaseExploreApp/BaseExploreApp.vue
@@ -3,17 +3,7 @@
  * Wrapper template for all Explore JPL App pages.
  * Provides the sticky navigation.
  */
-import { computed } from 'vue'
 import NavSecondary from './../../../components/NavSecondary/NavSecondary.vue'
-import type { BreadcrumbPathObject } from './../../../interfaces'
-
-/** Fallback nav data until we finalize how this will be provided by the backend */
-const staticNavData = [
-  { path: '/explore-app/', title: 'Explore JPL', children: [] },
-  { path: '/explore-app/maps/', title: 'Map', children: [] },
-  { path: '/explore-app/sites/', title: 'Sites', children: [] },
-  { path: '/explore-app/info/', title: 'Info', children: [] }
-] as BreadcrumbPathObject[]
 
 const props = defineProps({
   /** If hero content appears above the navigation (provided by parent component) */
@@ -21,21 +11,10 @@ const props = defineProps({
     type: Boolean,
     default: false
   },
-  /** Override static navigation (provided by parent component) */
   navData: {
-    type: [Array as () => BreadcrumbPathObject[], String as () => string],
+    type: String,
     default: undefined
   }
-})
-
-const computedNavData = computed(() => {
-  // read prop data. use static data as a fallback
-  let data: string | BreadcrumbPathObject[] = props.navData || staticNavData
-  // convert to a string
-  if (typeof data != 'string') {
-    data = JSON.stringify(data)
-  }
-  return data
 })
 </script>
 <template>
@@ -43,7 +22,7 @@ const computedNavData = computed(() => {
   <slot name="hero"></slot>
   <!-- navigation -->
   <NavSecondary
-    :breadcrumb="computedNavData"
+    :breadcrumb="navData"
     :has-intro="hasIntro"
     show-on-mobile
   />
---
+++ packages/vue/src/templates/explore-jpl/PageSiteExploreApp/PageSiteExploreApp.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+/** Explore JPL App's equivalent to PageContent */
+import { computed, defineProps, reactive } from 'vue'
+import type { PageObject } from '../../../interfaces'
+// import { useThemeStore } from '../../../store/theme'
+import BaseExploreApp from './../../explore-jpl/BaseExploreApp/BaseExploreApp.vue'
+
+// const themeStore = useThemeStore()
+
+const props = defineProps({
+  data: {
+    type: Object as () => PageObject,
+    default: undefined
+  }
+})
+const { data } = reactive(props)
+
+/** If the hero image is inline (treated as a regular block) */
+const heroInline = computed(() => {
+  if (data?.heroPosition === 'inline') {
+    return true
+  }
+  return false
+})
+/** If the page title should overlay the hero image (does not apply to inline heroes) */
+// const heroTitle = computed(() => {
+//   return data?.heroImage && !heroInline.value && data?.displayTitleInHero
+// })
+</script>
+<template>
+  <div
+    v-if="data"
+    class="ThemeVariantLight"
+    :class="{ '-nav-offset': !heroInline && data.heroImage }"
+  >
+    <BaseExploreApp
+      :has-intro="data.heroImage && !heroInline"
+      :nav-data="data.breadcrumb"
+    >
+      <h2>TODO</h2>
+    </BaseExploreApp>
+  </div>
+</template>
