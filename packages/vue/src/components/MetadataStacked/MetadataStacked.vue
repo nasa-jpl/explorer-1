dd4994d4a550c2d88365a8aa66d70932a40443c4
Steven Fimbres <32422862+sfimbres@users.noreply.github.com>
2026-01-22 16:37:26 -0800

    Update MetadataStacked.vue (#807)

--- packages/vue/src/components/MetadataStacked/MetadataStacked.vue
+++ packages/vue/src/components/MetadataStacked/MetadataStacked.vue
@@ -37,29 +37,25 @@ const props = defineProps({
 
 <template>
   <div class="MetadataStacked inline-flex flex-col flex-start gap-2.5 text-body-md">
-    <div class="flex flex-row gap-2.5 items-center self-stretch">
-      <svg
-        v-if="props.locationIcon === 'test'"
-        xmlns="http://www.w3.org/2000/svg"
-        width="28"
-        height="28"
-        viewBox="0 0 28 28"
-        fill="none"
-      >
-        <circle
-          cx="14"
-          cy="14"
-          r="14"
-          fill="#7C6500"
-        />
-      </svg>
+    <div
+      v-if="props.location"
+      class="flex flex-row gap-2.5 items-center self-stretch"
+    >
+      <img
+        v-if="props.locationIcon"
+        :src="props.locationIcon"
+        class="w-4 h-4"
+      />
       <IconLocation
         v-else
         class="MetadataStackedIcon text-jpl-red"
       />
       <span>{{ props.location }}</span>
     </div>
-    <div class="flex flex-row gap-2.5 items-center self-stretch">
+    <div
+      v-if="props.wait"
+      class="flex flex-row gap-2.5 items-center self-stretch"
+    >
       <IconHourglass class="MetadataStackedIcon text-jpl-red" />
       <span>{{ props.wait }}</span>
     </div>
