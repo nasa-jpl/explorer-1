74bdb84517774f6eacdb327ed018dffe5dcb512d
Stephanie Smith <stephiescastle@users.noreply.github.com>
2026-03-10 13:22:02 -0700

    moving a11y-dialog to devDependencies, using lodash-es instead of lodash (#846)
    * moving a11y-dialog to devDependencies, using lodash-es instead of lodash
    
    * updating lockfile

--- packages/vue/vite.config.ts
+++ packages/vue/vite.config.ts
@@ -37,13 +37,11 @@ export default defineConfig({
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled into your library
-      external: ['vue', 'lodash'],
+      external: ['vue'],
       output: {
         // Provide global variables to use in the UMD build for externalized deps
         globals: {
-          vue: 'Vue',
-          lodash: 'lodash',
-          'lodash/throttle': '_throttle'
+          vue: 'Vue'
         }
       }
     }
