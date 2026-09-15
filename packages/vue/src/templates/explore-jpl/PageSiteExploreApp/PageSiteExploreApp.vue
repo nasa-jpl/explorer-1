d356bb49ae8e48da3be30ef36d5255215a2c9656
Stephanie Smith <stephanie.c.smith@jpl.nasa.gov>
2026-09-14 17:57:25 -0700

    adjusting top margin of site detail page

--- packages/vue/src/templates/explore-jpl/PageSiteExploreApp/PageSiteExploreApp.vue
+++ packages/vue/src/templates/explore-jpl/PageSiteExploreApp/PageSiteExploreApp.vue
@@ -54,7 +54,7 @@ const mapPath = computed(() => {
         <LayoutHelper indent="col-2">
           <DetailHeadline
             :title="data.title"
-            class="mb-5"
+            class="my-5 lg:mt-10"
           />
           <MetadataStacked
             :location="data.location"
