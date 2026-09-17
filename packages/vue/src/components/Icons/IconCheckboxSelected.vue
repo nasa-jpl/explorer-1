e331fa3f7e9f00ded14de46d2f3647bc8a00f893
Steven T Fimbres <Steven.T.Fimbres@jpl.nasa.gov>
2026-09-17 14:31:27 -0700

    Update IconCheckboxSelected.vue

--- packages/vue/src/components/Icons/IconCheckboxSelected.vue
+++ packages/vue/src/components/Icons/IconCheckboxSelected.vue
@@ -1,27 +1,13 @@
 <template>
   <svg
-    class="IconCheckboxSelected"
-    width="22"
-    height="22"
-    viewBox="0 0 22 22"
+    class="IconBase"
     xmlns="http://www.w3.org/2000/svg"
-    aria-hidden="true"
-    focusable="false"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="currentColor"
   >
-    <g
-      fill="none"
-      fill-rule="evenodd"
-    >
-      <path
-        fill="currentColor"
-        fill-rule="nonzero"
-        d="M0 0H22V22H0z"
-      />
-      <path
-        d="M9.732 15.718l-4.95-4.95 1.415-1.414 3.535 3.535 7.071-7.071 1.415 1.414-8.486 8.486z"
-        fill="#FFF"
-      />
-    </g>
+    <path d="m424-312 282-282-56-56-226 226-114-114-56 56 170 170ZM120-120v-720h720v720H120Z" />
   </svg>
 </template>
 <script lang="ts">
@@ -31,5 +17,5 @@ export default defineComponent({
 })
 </script>
 <style lang="scss">
-@import '@explorer-1/common/src/scss/components/IconCheckboxSelected';
+@import '@explorer-1/common/src/scss/components/IconBase';
 </style>
