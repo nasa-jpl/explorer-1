9db76a5aafda532d1c97457cdad8c9e4788ed6b5
Steven T Fimbres <Steven.T.Fimbres@jpl.nasa.gov>
2026-09-17 14:31:18 -0700

    Update IconBriefcase.vue

--- packages/vue/src/components/Icons/IconBriefcase.vue
+++ packages/vue/src/components/Icons/IconBriefcase.vue
@@ -1,15 +1,14 @@
-<!-- Source: https://fontawesome.com/icons/briefcase?f=classic&s=light -->
 <template>
   <svg
-    aria-hidden="true"
-    focusable="false"
-    width="34"
-    height="34"
-    viewBox="0 0 34 34"
-    class="IconBriefcase"
+    class="IconBase"
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="currentColor"
   >
     <path
-      d="M27.625 8.438c1.846 0 3.375 1.529 3.375 3.374v13.5a3.365 3.365 0 0 1-3.375 3.375H7.375C5.477 28.688 4 27.212 4 25.313v-13.5a3.365 3.365 0 0 1 3.375-3.374h3.375V5.905a2.532 2.532 0 0 1 2.531-2.531h8.438c1.37 0 2.531 1.16 2.531 2.531v2.532h3.375ZM12.437 5.905v2.532h10.126V5.905c0-.422-.422-.843-.844-.843H13.28c-.474 0-.844.421-.844.843Zm16.876 19.407v-6.75h-7.594v3.375c0 .474-.422.843-.844.843h-6.75a.833.833 0 0 1-.844-.843v-3.375H5.687v6.75c0 .949.739 1.687 1.688 1.687h20.25a1.7 1.7 0 0 0 1.688-1.688Zm-14.344-4.22h5.062v-2.53H14.97v2.53Zm14.344-4.218v-5.063c0-.896-.791-1.687-1.688-1.687H7.375a1.7 1.7 0 0 0-1.688 1.688v5.062h23.625Z"
+      d="M80-120v-600h240v-160h320v160h240v600H80Zm320-600h160v-80H400v80Zm400 360H600v80H360v-80H160v160h640v-160Zm-360 0h80v-80h-80v80Zm-280-80h200v-80h240v80h200v-200H160v200Zm320 40Z"
     />
   </svg>
 </template>
@@ -19,3 +18,6 @@ export default defineComponent({
   name: 'IconBriefcase'
 })
 </script>
+<style lang="scss">
+@import '@explorer-1/common/src/scss/components/IconBase';
+</style>
