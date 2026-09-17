9b3fa346441163cb38c29344cf136e11d3d56a42
Steven T Fimbres <Steven.T.Fimbres@jpl.nasa.gov>
2026-09-17 14:31:01 -0700

    Update IconBookmark.vue

--- packages/vue/src/components/Icons/IconBookmark.vue
+++ packages/vue/src/components/Icons/IconBookmark.vue
@@ -1,25 +1,23 @@
 <template>
   <svg
+    class="IconBase"
     xmlns="http://www.w3.org/2000/svg"
-    class="IconBookmark"
-    width="16"
-    height="21"
-    viewBox="0 0 16 21"
-    fill="none"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="currentColor"
   >
-    >
     <path
-      d="M0 21L8 16.4062L16 21V0H0V21Z"
-      fill="#949494"
+      d="M200-120v-720h560v720L480-240 200-120Zm80-122 200-86 200 86v-518H280v518Zm0-518h400-400Z"
     />
   </svg>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
 export default defineComponent({
-  name: 'IconHourglass'
+  name: 'IconBookmark'
 })
 </script>
 <style lang="scss">
-@import '@explorer-1/common/src/scss/components/IconHourglass';
+@import '@explorer-1/common/src/scss/components/IconBase';
 </style>
