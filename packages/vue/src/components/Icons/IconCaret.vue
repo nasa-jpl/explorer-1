345d47d98364c4e1025d6ba818e85e33bc32c01c
Steven T Fimbres <Steven.T.Fimbres@jpl.nasa.gov>
2026-09-17 14:31:23 -0700

    Update IconCaret.vue

--- packages/vue/src/components/Icons/IconCaret.vue
+++ packages/vue/src/components/Icons/IconCaret.vue
@@ -7,20 +7,16 @@ export default defineComponent({
 </script>
 <template>
   <svg
-    class="IconCaret"
-    width="8"
-    height="14"
-    viewBox="0 0 8 14"
+    class="IconBase"
     xmlns="http://www.w3.org/2000/svg"
-    aria-hidden="true"
-    focusable="false"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="currentColor"
   >
-    <path
-      d="M7.864 7.004L1.5 13.368.086 11.954l4.948-4.95-4.948-4.95L1.5.64l6.364 6.364z"
-      fill="currentColor"
-    />
+    <path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z" />
   </svg>
 </template>
 <style lang="scss">
-@import '@explorer-1/common/src/scss/components/IconCaret';
+@import '@explorer-1/common/src/scss/components/IconBase';
 </style>
