093fd8f5ef1a24dbc4b68d73dfd7dd515b39f015
Steven T Fimbres <Steven.T.Fimbres@jpl.nasa.gov>
2026-09-17 14:32:22 -0700

    Update IconInfo.vue

--- packages/vue/src/components/Icons/IconInfo.vue
+++ packages/vue/src/components/Icons/IconInfo.vue
@@ -1,16 +1,14 @@
 <template>
   <svg
-    class="IconInfo"
-    width="10"
-    height="22"
-    viewBox="0 0 10 22"
+    class="IconBase"
     xmlns="http://www.w3.org/2000/svg"
-    aria-hidden="true"
-    focusable="false"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="currentColor"
   >
     <path
-      d="M7 8v12h3v2H0v-2h3V10H0V8h7zM5 0a2.5 2.5 0 110 5 2.5 2.5 0 010-5z"
-      fill="currentColor"
+      d="M440-280h80v-240h-80v240Zm68.5-331.5Q520-623 520-640t-11.5-28.5Q497-680 480-680t-28.5 11.5Q440-657 440-640t11.5 28.5Q463-600 480-600t28.5-11.5ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"
     />
   </svg>
 </template>
@@ -22,5 +20,5 @@ export default defineComponent({
 })
 </script>
 <style lang="scss">
-@import '@explorer-1/common/src/scss/components/IconInfo';
+@import '@explorer-1/common/src/scss/components/IconBase';
 </style>
